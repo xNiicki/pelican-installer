76243c74c1c0cd34c3541cecfec236c8d90f03d5
Nick <developer@nick-lorenz.de>
2024-10-13 16:22:34 +0200

    update permissions.

--- panel.sh
+++ panel.sh
@@ -3,7 +3,7 @@
 
 apt update
 apt install software-properties-common lsb-release apt-transport-https ca-certificates net-tools sudo -y
-sudo su
+su
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
 apt update
