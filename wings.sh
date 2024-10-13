cd7b9316fed13197a7793ac04157745ae4bb0c84
Nick <developer@nick-lorenz.de>
2024-10-13 20:48:28 +0200

    Install wings

--- wings.sh
+++ wings.sh
@@ -6,3 +6,7 @@ apt install curl -y
 curl -sSL https://get.docker.com/ | CHANNEL=stable sh
 
 systemctl enable --now docker
+
+mkdir -p /etc/pelican /var/run/wings
+curl -L -o /usr/local/bin/wings "https://github.com/pelican-dev/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+chmod u+x /usr/local/bin/wings
